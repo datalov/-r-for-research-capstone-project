9cabc2a437b3ffb680e66fd7ba87fda3d7b33d13
“datalov” <bundorthomas@gmail.com>
2024-09-30 11:46:44 +0000

    commencing

--- Thomas' Capstone Project.R
+++ Thomas' Capstone Project.R
@@ -1,3 +1,15 @@
 library(tidyverse)
 library(knitr)
 cuckoo_data <- read.csv("https://raw.githubusercontent.com/xrander/bootcamp-test/master/data/cuckoo.csv") 
+str(cuckoo_data)
+
+cuckoo_data |>
+  group_by(species) |> 
+  summarize( 
+    frequency = n(), 
+    Mean_bth = mean(breadth), 
+    Median_bth = median(breadth), 
+    Mean_lth = mean(length), 
+    Median_lth = median(length) 
+  ) |> 
+  kable()
